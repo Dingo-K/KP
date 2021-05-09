a81c9888a00abf97bf3f6c7d737a7436397ae546
Dingo-K <denis49232@mail.ru>
2021-05-09 21:18:28 +0300

    new changed

--- Parking/Parking/MainWindow.xaml.cs
+++ Parking/Parking/MainWindow.xaml.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -105,19 +107,111 @@ namespace Parking
         {
             using(KPContext kP = new KPContext())
             {
+                PlaceClear();
                 var ForEnterOrExit = kP.Database.SqlQuery<Booking>($"select * from Booking where Booking.UserID = '{UserId.Text}'");
-                if(ForEnterOrExit != null)
+                try
                 {
-                    var info = ForEnterOrExit.Last();
-                    if(info.TimeEnd != null)
+                    if (ForEnterOrExit != null)
                     {
-                        
-                    }
-                    if (info.TimeEnd == null)
-                    {
+                        var info = ForEnterOrExit.LastOrDefault();
+                        var PlaceInfo = kP.Place.ToList();
+                        if (info != null)
+                        {
+                            if (info.TimeEnd != null) //Парковка
+                            {
+                                foreach (var i in PlaceInfo)
+                                {
+                                    if (i.Status == true)
+                                    {
+                                        //int intodata = kP.Database.ExecuteSqlCommand($"insert into Booking (UserID,PlaceID,TimeStart) values ({Convert.ToInt32(UserId.Text)},{Convert.ToInt32(i.PlaceId)},'GETDATE()'");
+                                        Booking booking = new Booking();
+                                        booking.UserID = Convert.ToInt32(UserId.Text);
+                                        booking.PlaceID = Convert.ToInt32(i.PlaceId);
+                                        booking.TimeStart = GetNetworkDateTime();
+                                        kP.Booking.Add(booking);
+                                        kP.SaveChanges();
+                                        int upd = kP.Database.ExecuteSqlCommand($"Update Place set Status = 'false' where PlaceId = {i.PlaceId}");
+                                        MessageBox.Show("Проезжайте к месту и сектору указаному ниже");
+                                        PlaceSector.Text = "Сектор: " + i.Sector;
+                                        PlaceNumber.Text = "Номер: " + i.Number;
+                                        break;
+                                    }
+                                }
 
+
+                            }
+                            if (info.TimeEnd == null) // выезд из парковки
+                            {
+                                DateTime dateTime = new DateTime();
+                                dateTime = GetNetworkDateTime();
+                                int chnge = kP.Database.ExecuteSqlCommand($"Update Booking set TimeEnd = '{dateTime}' where BookingId = {Convert.ToInt32(info.BookingId)}");
+                                int upd2 = kP.Database.ExecuteSqlCommand($"Update Place set Status = 'true' where PlaceId = {Convert.ToInt32(info.PlaceID)}");
+                                var aboutcost = PlaceInfo.Last();
+                                PlaceSector.Text = "Время стоянки: " + Convert.ToString(CheckTime(info.TimeStart, (DateTime)info.TimeEnd)) + " минут";
+                                PlaceNumber.Text = "Стоимость: " + Convert.ToString(CheckTime(info.TimeStart, (DateTime)info.TimeEnd) * aboutcost.cost);
+                            }
+                        }
+                        if (info == null)
+                        {
+                            foreach (var i in PlaceInfo)
+                            {
+                                if (i.Status == true)
+                                {
+                                    //int intodata = kP.Database.ExecuteSqlCommand($"insert into Booking (UserID,PlaceID,TimeStart) values ({Convert.ToInt32(UserId.Text)},{Convert.ToInt32(i.PlaceId)},'GETDATE()'");
+                                    Booking booking = new Booking();
+                                    booking.UserID = Convert.ToInt32(UserId.Text);
+                                    booking.PlaceID = Convert.ToInt32(i.PlaceId);
+                                    booking.TimeStart = GetNetworkDateTime();
+                                    kP.Booking.Add(booking);
+                                    kP.SaveChanges();
+                                    int upd3 = kP.Database.ExecuteSqlCommand($"Update Place set Status = 'false' where PlaceId = {i.PlaceId}");
+                                    MessageBox.Show("Проезжайте к месту и сектору указаному ниже");
+                                    PlaceSector.Text = "Сектор: " + i.Sector;
+                                    PlaceNumber.Text = "Номер: " + i.Number;
+                                    break;
+                                }
+                            }
+                        }
                     }
+
                 }
+                catch (Exception)
+                {
+                    MessageBox.Show("Попробуйте в другое время");
+                }
+            }
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PlaceClear()
+        {
+            PlaceNumber.Text = " ";
+            PlaceSector.Text = " ";
+        }
+        private double CheckTime(DateTime start, DateTime end)
+        {
+            TimeSpan span = new TimeSpan();
+            span = end.Subtract(start);
+            return span.TotalMinutes;
+        }
+        public static DateTime GetNetworkDateTime()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+
+                socket.Connect("time.nist.gov", 13);
+                using (StreamReader rstream = new StreamReader(new NetworkStream(socket)))
+                {
+                    string value = rstream.ReadToEnd().Trim();
+                    MatchCollection matches = Regex.Matches(value, @"((\d*)-(\d*)-(\d*))|((\d*):(\d*):(\d*))");
+                    string[] dd = matches[0].Value.Split('-');
+                    return DateTime.Parse($"{matches[1].Value} {dd[2]}.{dd[1]}.{dd[0]}");
+                }
+
             }
         }
     }
